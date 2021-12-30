b923bab47aef42022e5887cea065b34508d1347d
yamada.masahiro <yamada.masahiro@gmail.com>
2021-12-30 07:12:48 +0000

    api fix and working.

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,8 +1,13 @@
 import {useCallback, useState} from "react";
-//import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import API from "@aws-amplify/api";
+import Amplify from "aws-amplify";
 
+//import axios from "axios";
+import config from "../aws-exports";
 import { useMessage } from "./useMessage";
+
+Amplify.configure(config);
 
 export const useAuth = () => {
   const navigate = useNavigate();
@@ -11,13 +16,34 @@ export const useAuth = () => {
   
   const login = useCallback((id) => {
     setLoading(true);
-    console.log("useAuth1:","id")
-    showMessage({
-      title: "ログインしました",
-      status: "success"
-    });
-    navigate("/home");
-    setLoading(false);
+    API.get('kuzanboapi',`/books/login?query="user_id=${id}"`)
+    .then(async res => {
+      alert(res.success);
+      alert(JSON.stringify(res.data));
+      if(res.success){
+        showMessage({
+          title: "ログインしました",
+          status: "success"
+        })
+        navigate("/home");
+        setLoading(false);
+      }else{
+        showMessage({
+          title: "ユーザが存在しません",
+          status: "error"
+        });
+        navigate("/");
+        setLoading(false);
+      }
+    })
+    .catch(() => {
+      showMessage({
+        title: "接続に失敗しました",
+        status: "error"
+      });
+      navigate("/");
+      setLoading(false);
+    })
   },[]);
   return {login, loading};
 };
